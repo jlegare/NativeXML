362558441d84b2713288eee666b030667ef1be92
Jacques Légaré <jacques.legare70@gmail.com>
2019-03-27 12:31:24 -0400

    Added support for CDATA marked sections.

--- src/Events.jl
+++ src/Events.jl
@@ -23,8 +23,15 @@ import .Lexical
 # TYPE DECLARATIONS
 # ----------------------------------------
 
+struct CDATAMarkedSection
+    value          ::String
+    identification ::String
+    line_number    ::Int64
+end
+
+
 struct CharacterReference
-    value          ::String # It's someone else's job to verify that the value is a legititmate character.
+    value          ::String # It's someone else's job to verify that the value is a legitimate character.
     identification ::String
     line_number    ::Int64
 end
@@ -38,7 +45,7 @@ end
 
 
 struct ProcessingInstruction
-    target         ::String
+    target         ::String # It's someone else's job to verify that this isn't some case variant of "XML".
     value          ::String
     identification ::String
     line_number    ::Int64
@@ -60,7 +67,10 @@ function events(state::Lexical.State)
     tokens = Lexical.tokens(state)
 
     while true
-        if is_token(Lexical.cro, tokens)
+        if is_token(Lexical.mdo, tokens)
+            @show markup_declaration(tokens)
+
+        elseif is_token(Lexical.cro, tokens)
             @show character_reference(tokens)
 
         elseif is_token(Lexical.ero, tokens)
@@ -73,6 +83,65 @@ function events(state::Lexical.State)
             @info "NOPE!"
             break
         end
+    end
+end
+
+
+function cdata_marked_section(mdo, tokens)
+    dso = take!(tokens)
+
+    if is_token(Lexical.text, tokens)
+        text = fetch(tokens)
+
+        if text.value == "CDATA"
+            take!(tokens)
+
+            if is_token(Lexical.dso, tokens)
+                take!(tokens)
+
+                consumed = Array{Lexical.Token, 1}()
+
+                while true
+                    if is_token(Lexical.msc, tokens)
+                        msc = take!(tokens)
+
+                        if is_token(Lexical.tagc, tokens)
+                            return CDATAMarkedSection(join(map(value -> value.value, consumed), ""),
+                                                      Lexical.location_of(mdo)...)
+
+                        else
+                            t = vcat(mdo, dso, text, consumed, msc)
+
+                            return MarkupError("ERROR: Expecting '>' to end a CDATA marked section.", t,
+                                               Lexical.location_of(t[end])...)
+                        end
+
+                    elseif is_eoi(tokens)
+                        t = vcat(mdo, dso, text, consumed)
+
+                        return MarkupError("ERROR: Expecting ']]>' to end a CDATA marked section.", t,
+                                           Lexical.location_of(t[end])...)
+
+                    else
+                        push!(consumed, take!(tokens))
+                    end
+                end
+
+                # Handle the content of a CDATA marked section here.
+
+            else
+                return MarkupError("ERROR: Expecting '[' to open a CDATA marked section.", [ mdo, dso, text ],
+                                   Lexical.location_of(text)...)
+            end
+
+        else
+            return MarkupError("ERROR: Expecting 'CDATA' to open a CDATA marked section.", [ mdo, dso ],
+                               Lexical.location_of(dso)...)
+        end
+
+    else
+        return MarkupError("ERROR: Expecting 'CDATA' to open a CDATA marked section.", [ mdo, dso ],
+                           Lexical.location_of(dso)...)
     end
 end
 
@@ -134,11 +203,24 @@ function is_token(token_type, tokens)
 end
 
 
+function markup_declaration(tokens)
+    mdo = take!(tokens) # Consume the MDO token that got us here.
+
+    if is_token(Lexical.dso, tokens)
+        return cdata_marked_section(mdo, tokens)
+
+    else
+        return MarkupError("ERROR: Expecting the start of a markup declaration.", [ mdo ], Lexical.location_of(mdo)...)
+    end
+end
+
+
 function processing_instruction(tokens)::Union{ProcessingInstruction, MarkupError}
     pio = take!(tokens) # Consume the PIO token that got us here.
 
     if is_token(Lexical.text, tokens)
-        target = take!(tokens) # See [1], § 2.6 ... the PI target is required.
+        target = take!(tokens) # See [1], § 2.6 ... the PI target is required. Technically, we should exclude all case
+                               # variants of "XML" from the PI target, but let's leave that to another layer for now.
 
         if is_token(Lexical.ws, tokens)
             take!(tokens) # Don't bother holding on to this one. Again, see [1], § 2.6 ... the white space is required,
