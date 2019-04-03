77cecad593ed5c11cdf7bb53b9cf9d9dba70d8cd
Jacques Légaré <jacques.legare70@gmail.com>
2019-04-03 18:03:04 -0400

    Added negative tests for comments.

--- test/events/comments.jl
+++ test/events/comments.jl
@@ -32,4 +32,59 @@
                     E.DataContent(" declarations for <head> & <body> ", "a buffer", -1),
                     E.CommentEnd("a buffer", -1) ])
     end
+
+    @testset "Events/Comments (Negative ... no terminator)" begin
+        @test (collect(E.events(L.State(IOBuffer("<!--")))) 
+               == [ E.CommentStart("a buffer", -1),
+                    E.DataContent("", "a buffer", -1),
+                    E.MarkupError("ERROR: Expecting '-->' to end a comment.", [ ], "a buffer", -1),
+                    E.CommentEnd(true, "a buffer", -1) ])
+        @test (collect(E.events(L.State(IOBuffer("<!--x")))) 
+               == [ E.CommentStart("a buffer", -1),
+                    E.DataContent("x", "a buffer", -1),
+                    E.MarkupError("ERROR: Expecting '-->' to end a comment.", [ ], "a buffer", -1),
+                    E.CommentEnd(true, "a buffer", -1) ])
+        @test (collect(E.events(L.State(IOBuffer("<!--é")))) 
+               == [ E.CommentStart("a buffer", -1),
+                    E.DataContent("é", "a buffer", -1),
+                    E.MarkupError("ERROR: Expecting '-->' to end a comment.", [ ], "a buffer", -1),
+                    E.CommentEnd(true, "a buffer", -1) ])
+        @test (collect(E.events(L.State(IOBuffer("<!-- declarations for <head> & <body> ")))) 
+               == [ E.CommentStart("a buffer", -1),
+                    E.DataContent(" declarations for <head> & <body> ", "a buffer", -1),
+                    E.MarkupError("ERROR: Expecting '-->' to end a comment.", [ ], "a buffer", -1),
+                    E.CommentEnd(true, "a buffer", -1) ])
+
+        @test (collect(E.events(L.State(IOBuffer("<!--x-")))) 
+               == [ E.CommentStart("a buffer", -1),
+                    E.DataContent("x-", "a buffer", -1),
+                    E.MarkupError("ERROR: Expecting '-->' to end a comment.", [ ], "a buffer", -1),
+                    E.CommentEnd(true, "a buffer", -1) ])
+        @test (collect(E.events(L.State(IOBuffer("<!--x--")))) 
+               == [ E.CommentStart("a buffer", -1),
+                    E.DataContent("x", "a buffer", -1),
+                    E.MarkupError("ERROR: Expecting '-->' to end a comment.", [ ], "a buffer", -1),
+                    E.CommentEnd(true, "a buffer", -1) ])
+    end
+
+    @testset "Events/Comments (Negative ... nested --)" begin
+        @test (collect(E.events(L.State(IOBuffer("<!-- -- -->")))) 
+               == [ E.CommentStart("a buffer", -1),
+                    E.DataContent(" ", "a buffer", -1),
+                    E.MarkupError("ERROR: '--' is not allowed inside a comment.", [ ], "a buffer", -1),
+                    E.CommentEnd(true, "a buffer", -1) ])
+        @test (collect(E.events(L.State(IOBuffer("<!------>")))) 
+               == [ E.CommentStart("a buffer", -1),
+                    E.DataContent("", "a buffer", -1),
+                    E.MarkupError("ERROR: '--' is not allowed inside a comment.", [ ], "a buffer", -1),
+                    E.CommentEnd(true, "a buffer", -1) ])
+
+        # This is from the XML specification, § 2.5.
+        #
+        @test (collect(E.events(L.State(IOBuffer("<!-- B+, B, or B--->")))) 
+               == [ E.CommentStart("a buffer", -1),
+                    E.DataContent(" B+, B, or B", "a buffer", -1),
+                    E.MarkupError("ERROR: '--' is not allowed inside a comment.", [ ], "a buffer", -1),
+                    E.CommentEnd(true, "a buffer", -1) ])
+    end
 end
