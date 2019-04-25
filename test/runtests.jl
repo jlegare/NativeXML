ff16906cee768ab1a30ca32746835a0dc4bbeb00
Jacques Légaré <jacques.legare70@gmail.com>
2019-04-25 16:38:11 -0400

    Added a few positive tests of document type declaration parsing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using NativeXML.Lexical
 using NativeXML.Events
 
 tests = [ "lexical", 
-          "events/basic", "events/cdata_ms", "events/comments",
+          "events/basic", "events/cdata_ms", "events/comments", "events/doctype",
           "events/element_end", "events/element_start", "events/attributes",
           "events/entities", "events/pis", ]
 
