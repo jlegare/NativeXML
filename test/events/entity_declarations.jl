fe5a27687537a1ba6c35c4ca711f113db8e4da52
Jacques Légaré <jacques.legare70@gmail.com>
2019-12-01 16:32:23 -0500

    Introduced aliases to make these tests a bit less verbose.

--- test/events/entity_declarations.jl
+++ test/events/entity_declarations.jl
@@ -4,45 +4,46 @@
     E = Events
     L = Events.Lexical
 
+    InternalGeneral   = E.EntityDeclarationInternalGeneral
+    InternalParameter = E.EntityDeclarationInternalParameter
+
     @testset "Events/Entity Declarations, Internal General (Positive)" begin
         # Basic tests ... empty entity value.
         #
-        @test evaluate("<!ENTITY a \"\">")   == [ E.EntityDeclarationInternalGeneral("a", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY abc \"\">") == [ E.EntityDeclarationInternalGeneral("abc", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY é \"\">")   == [ E.EntityDeclarationInternalGeneral("é", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY aé \"\">")  == [ E.EntityDeclarationInternalGeneral("aé", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY éb \"\">")  == [ E.EntityDeclarationInternalGeneral("éb", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY aéb \"\">") == [ E.EntityDeclarationInternalGeneral("aéb", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY\na\n\"\">") == [ E.EntityDeclarationInternalGeneral("a", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY\na\t\"\">") == [ E.EntityDeclarationInternalGeneral("a", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY a \"\">")   == [ InternalGeneral("a", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY abc \"\">") == [ InternalGeneral("abc", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY é \"\">")   == [ InternalGeneral("é", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY aé \"\">")  == [ InternalGeneral("aé", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY éb \"\">")  == [ InternalGeneral("éb", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY aéb \"\">") == [ InternalGeneral("aéb", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY\na\n\"\">") == [ InternalGeneral("a", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY\na\t\"\">") == [ InternalGeneral("a", "", L.Location("a buffer", -1)) ]
 
         # This is from the XML specification, § 4.2.1 ...
         #
         @test (evaluate("<!ENTITY Pub-Status \"This is a pre-release of the\nspecification.\">")
-               == [ E.EntityDeclarationInternalGeneral("Pub-Status", "This is a pre-release of the\nspecification.",
-                                                       L.Location("a buffer", -1)) ])
+               == [ InternalGeneral("Pub-Status", "This is a pre-release of the\nspecification.", L.Location("a buffer", -1)) ])
 
         # This is from the XML specification, § 4.4.5 ...
         #
         @test (evaluate("<!ENTITY WhatHeSaid \"He said %YN;\" >")
-               == [ E.EntityDeclarationInternalGeneral("WhatHeSaid", "He said %YN;", L.Location("a buffer", -1)) ])
+               == [ InternalGeneral("WhatHeSaid", "He said %YN;", L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/Entity Declarations, Internal Parameter (Positive)" begin
         # Basic tests ... empty entity value.
         #
-        @test evaluate("<!ENTITY % a \"\">")    == [ E.EntityDeclarationInternalParameter("a", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY % abc \"\">")  == [ E.EntityDeclarationInternalParameter("abc", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY % é \"\">")    == [ E.EntityDeclarationInternalParameter("é", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY % aé \"\">")   == [ E.EntityDeclarationInternalParameter("aé", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY % éb \"\">")   == [ E.EntityDeclarationInternalParameter("éb", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY % aéb \"\">")  == [ E.EntityDeclarationInternalParameter("aéb", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY\n%\na\n\"\">") == [ E.EntityDeclarationInternalParameter("a", "", L.Location("a buffer", -1)) ]
-        @test evaluate("<!ENTITY\n%\ta\t\"\">") == [ E.EntityDeclarationInternalParameter("a", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY % a \"\">")    == [ InternalParameter("a", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY % abc \"\">")  == [ InternalParameter("abc", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY % é \"\">")    == [ InternalParameter("é", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY % aé \"\">")   == [ InternalParameter("aé", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY % éb \"\">")   == [ InternalParameter("éb", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY % aéb \"\">")  == [ InternalParameter("aéb", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY\n%\na\n\"\">") == [ InternalParameter("a", "", L.Location("a buffer", -1)) ]
+        @test evaluate("<!ENTITY\n%\ta\t\"\">") == [ InternalParameter("a", "", L.Location("a buffer", -1)) ]
 
         # This is from the XML specification, § 4.4.5 ...
         #
-        @test (evaluate("<!ENTITY % YN \'\"Yes\"\' >")
-               == [ E.EntityDeclarationInternalParameter("YN", "\"Yes\"", L.Location("a buffer", -1)) ])
+        @test evaluate("<!ENTITY % YN \'\"Yes\"\' >") == [ InternalParameter("YN", "\"Yes\"", L.Location("a buffer", -1)) ]
     end
 end
