4d7dc9c3fd5bacf968e75c530a36c6d57a67edb7
Jacques Légaré <jacques.legare70@gmail.com>
2019-12-10 09:19:44 -0500

    Made these negative tests verify recovery.

--- test/events/cdata_ms.jl
+++ test/events/cdata_ms.jl
@@ -4,140 +4,151 @@
     E = Events
     L = Events.Lexical
 
-    @testset "Events/CDATA Marked Section (Positive)" begin
-        @test evaluate("<![CDATA[]]>") == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                                            E.DataContent("", L.Location("a buffer", -1)),
-                                            E.CDATAMarkedSectionEnd(L.Location("a buffer", -1)) ]
+    CDataStart = E.CDATAMarkedSectionStart
+    CDataEnd   = E.CDATAMarkedSectionEnd
 
-        @test evaluate("<![CDATA[a]]>") == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                                             E.DataContent("a", L.Location("a buffer", -1)),
-                                             E.CDATAMarkedSectionEnd(L.Location("a buffer", -1)) ]
-        @test evaluate("<![CDATA[abc]]>") == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                                               E.DataContent("abc", L.Location("a buffer", -1)),
-                                               E.CDATAMarkedSectionEnd(L.Location("a buffer", -1)) ]
-        @test evaluate("<![CDATA[é]]>") == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                                             E.DataContent("é", L.Location("a buffer", -1)),
-                                             E.CDATAMarkedSectionEnd(L.Location("a buffer", -1)) ]
-        @test evaluate("<![CDATA[aé]]>") == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                                              E.DataContent("aé", L.Location("a buffer", -1)),
-                                              E.CDATAMarkedSectionEnd(L.Location("a buffer", -1)) ]
-        @test evaluate("<![CDATA[éb]]>") == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                                              E.DataContent("éb", L.Location("a buffer", -1)),
-                                              E.CDATAMarkedSectionEnd(L.Location("a buffer", -1)) ]
-        @test evaluate("<![CDATA[aéb]]>") == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                                               E.DataContent("aéb", L.Location("a buffer", -1)),
-                                               E.CDATAMarkedSectionEnd(L.Location("a buffer", -1)) ]
+    DC = E.DataContent
+    ME = E.MarkupError
+
+    @testset "Events/CDATA Marked Section (Positive)" begin
+        @test evaluate("<![CDATA[]]>") == [ CDataStart(L.Location("a buffer", -1)),
+                                            DC("", L.Location("a buffer", -1)),
+                                            CDataEnd(L.Location("a buffer", -1)) ]
+
+        @test evaluate("<![CDATA[a]]>") == [ CDataStart(L.Location("a buffer", -1)),
+                                             DC("a", L.Location("a buffer", -1)),
+                                             CDataEnd(L.Location("a buffer", -1)) ]
+        @test evaluate("<![CDATA[abc]]>") == [ CDataStart(L.Location("a buffer", -1)),
+                                               DC("abc", L.Location("a buffer", -1)),
+                                               CDataEnd(L.Location("a buffer", -1)) ]
+        @test evaluate("<![CDATA[é]]>") == [ CDataStart(L.Location("a buffer", -1)),
+                                             DC("é", L.Location("a buffer", -1)),
+                                             CDataEnd(L.Location("a buffer", -1)) ]
+        @test evaluate("<![CDATA[aé]]>") == [ CDataStart(L.Location("a buffer", -1)),
+                                              DC("aé", L.Location("a buffer", -1)),
+                                              CDataEnd(L.Location("a buffer", -1)) ]
+        @test evaluate("<![CDATA[éb]]>") == [ CDataStart(L.Location("a buffer", -1)),
+                                              DC("éb", L.Location("a buffer", -1)),
+                                              CDataEnd(L.Location("a buffer", -1)) ]
+        @test evaluate("<![CDATA[aéb]]>") == [ CDataStart(L.Location("a buffer", -1)),
+                                               DC("aéb", L.Location("a buffer", -1)),
+                                               CDataEnd(L.Location("a buffer", -1)) ]
 
         # This is from the XML specification, § 2.7.
         #
         @test (evaluate("<![CDATA[<greeting>Hello, world!</greeting>]]>")
-               == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                    E.DataContent("<greeting>Hello, world!</greeting>", L.Location("a buffer", -1)),
-                    E.CDATAMarkedSectionEnd(L.Location("a buffer", -1)) ])
+               == [ CDataStart(L.Location("a buffer", -1)),
+                    DC("<greeting>Hello, world!</greeting>", L.Location("a buffer", -1)),
+                    CDataEnd(L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/CDATA Marked Section (Negative ... no section type (CDATA))" begin
         @test (evaluate("<![")
-               == [ E.MarkupError("ERROR: Expecting 'CDATA' to open a CDATA marked section.",
-                                  [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
-                                    L.Token(L.dso, "[", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
+               == [ ME("ERROR: Expecting 'CDATA' to open a CDATA marked section.",
+                       [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
+                         L.Token(L.dso, "[", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
 
         # White space is not allowed between the DSO and the CDATA token.
         #
         @test (evaluate("<![ CDATA")
-               == [ E.MarkupError("ERROR: Expecting 'CDATA' to open a CDATA marked section.",
-                                  [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
-                                    L.Token(L.dso, "[", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
-                    E.DataContent(" ", true, L.Location("a buffer", -1)),
-                    E.DataContent("CDATA", false, L.Location("a buffer", -1)) ])
+               == [ ME("ERROR: Expecting 'CDATA' to open a CDATA marked section.",
+                       [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
+                         L.Token(L.dso, "[", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                    DC(" ", true, L.Location("a buffer", -1)),
+                    DC("CDATA", false, L.Location("a buffer", -1)) ])
 
         @test (evaluate("<![ ")
-               == [ E.MarkupError("ERROR: Expecting 'CDATA' to open a CDATA marked section.",
-                                  [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
-                                    L.Token(L.dso, "[", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
-                    E.DataContent(" ", true, L.Location("a buffer", -1)) ])
+               == [ ME("ERROR: Expecting 'CDATA' to open a CDATA marked section.",
+                       [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
+                         L.Token(L.dso, "[", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                    DC(" ", true, L.Location("a buffer", -1)) ])
 
-        # Check that a random token is caught. But careful ... the parser keeps going, so we only check the FIRST
-        # event. (Otherwise we're testing the results of some other part of the parser.)
+        # Check that a random token is caught.
         #
         events = evaluate("<![<")
-        @test length(events) > 1
-        @test (events[1] == E.MarkupError("ERROR: Expecting 'CDATA' to open a CDATA marked section.",
-                                          [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
-                                            L.Token(L.dso, "[", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)))
+        @test length(events) >= 2
+        @test (events[1:2] == [ ME("ERROR: Expecting 'CDATA' to open a CDATA marked section.",
+                                   [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
+                                     L.Token(L.dso, "[", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                                ME("ERROR: Expecting an element name.",
+                                   [ L.Token(L.stago, "<", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/CDATA Marked Section (Negative ... wrong section type (CDATA))" begin
         @test (evaluate("<![SDATA")
-               == [ E.MarkupError("ERROR: Expecting 'CDATA' to open a CDATA marked section.",
-                                  [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
-                                    L.Token(L.dso, "[", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
-                    E.DataContent("SDATA", false, L.Location("a buffer", -1)) ])
+               == [ ME("ERROR: Expecting 'CDATA' to open a CDATA marked section.",
+                       [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
+                         L.Token(L.dso, "[", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                    DC("SDATA", false, L.Location("a buffer", -1)) ])
+
         @test (evaluate("<![cdata")
-               == [ E.MarkupError("ERROR: Expecting 'CDATA' to open a CDATA marked section.",
-                                  [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
-                                    L.Token(L.dso, "[", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
-                    E.DataContent("cdata", false, L.Location("a buffer", -1)) ])
+               == [ ME("ERROR: Expecting 'CDATA' to open a CDATA marked section.",
+                       [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
+                         L.Token(L.dso, "[", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                    DC("cdata", false, L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/CDATA Marked Section (Negative ... no second DSO)" begin
         @test (evaluate("<![CDATA")
-               == [ E.MarkupError("ERROR: Expecting '[' to open a CDATA marked section.",
-                                  [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
-                                    L.Token(L.dso, "[", L.Location("a buffer", -1)),
-                                    L.Token(L.text, "CDATA", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)), ])
+               == [ ME("ERROR: Expecting '[' to open a CDATA marked section.",
+                       [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
+                         L.Token(L.dso, "[", L.Location("a buffer", -1)),
+                         L.Token(L.text, "CDATA", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)), ])
 
         # White space is not allowed between the CDATA token and the second DSO.
         #
         @test (evaluate("<![CDATA [")
-               == [ E.MarkupError("ERROR: Expecting '[' to open a CDATA marked section.",
-                                  [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
-                                    L.Token(L.dso, "[", L.Location("a buffer", -1)),
-                                    L.Token(L.text, "CDATA", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
-                    E.DataContent(" ", true, L.Location("a buffer", -1)),
-                    E.DataContent("[", false, L.Location("a buffer", -1)), ])
+               == [ ME("ERROR: Expecting '[' to open a CDATA marked section.",
+                       [ L.Token(L.mdo, "<!", L.Location("a buffer", -1)),
+                         L.Token(L.dso, "[", L.Location("a buffer", -1)),
+                         L.Token(L.text, "CDATA", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                    DC(" ", true, L.Location("a buffer", -1)),
+                    DC("[", false, L.Location("a buffer", -1)), ])
     end
 
     @testset "Events/CDATA Marked Section (Negative ... no MSC)" begin
         @test (evaluate("<![CDATA[")
-               == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                    E.DataContent("", L.Location("a buffer", -1)),
-                    E.MarkupError("ERROR: Expecting ']]>' to end a CDATA marked section.", [ ], L.Location("a buffer", -1)),
-                    E.CDATAMarkedSectionEnd(true, L.Location("a buffer", -1)) ])
+               == [ CDataStart(L.Location("a buffer", -1)),
+                    DC("", L.Location("a buffer", -1)),
+                    ME("ERROR: Expecting ']]>' to end a CDATA marked section.", [ ], L.Location("a buffer", -1)),
+                    CDataEnd(true, L.Location("a buffer", -1)) ])
+
         @test (evaluate("<![CDATA[Hello, World!")
-               == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                    E.DataContent("Hello, World!", L.Location("a buffer", -1)),
-                    E.MarkupError("ERROR: Expecting ']]>' to end a CDATA marked section.", [ ], L.Location("a buffer", -1)),
-                    E.CDATAMarkedSectionEnd(true, L.Location("a buffer", -1)) ])
+               == [ CDataStart(L.Location("a buffer", -1)),
+                    DC("Hello, World!", L.Location("a buffer", -1)),
+                    ME("ERROR: Expecting ']]>' to end a CDATA marked section.", [ ], L.Location("a buffer", -1)),
+                    CDataEnd(true, L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/CDATA Marked Section (Negative ... no TAGC)" begin
         @test (evaluate("<![CDATA[]]")
-               == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                    E.DataContent("", L.Location("a buffer", -1)),
-                    E.MarkupError("ERROR: Expecting '>' to end a CDATA marked section.", [ ], L.Location("a buffer", -1)),
-                    E.CDATAMarkedSectionEnd(true, L.Location("a buffer", -1)) ])
+               == [ CDataStart(L.Location("a buffer", -1)),
+                    DC("", L.Location("a buffer", -1)),
+                    ME("ERROR: Expecting '>' to end a CDATA marked section.", [ ], L.Location("a buffer", -1)),
+                    CDataEnd(true, L.Location("a buffer", -1)) ])
+
         @test (evaluate("<![CDATA[Hello, World!]]")
-               == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                    E.DataContent("Hello, World!", L.Location("a buffer", -1)),
-                    E.MarkupError("ERROR: Expecting '>' to end a CDATA marked section.", [ ], L.Location("a buffer", -1)),
-                    E.CDATAMarkedSectionEnd(true, L.Location("a buffer", -1)) ])
+               == [ CDataStart(L.Location("a buffer", -1)),
+                    DC("Hello, World!", L.Location("a buffer", -1)),
+                    ME("ERROR: Expecting '>' to end a CDATA marked section.", [ ], L.Location("a buffer", -1)),
+                    CDataEnd(true, L.Location("a buffer", -1)) ])
 
         # White space is not allowed between the MSC adn the TAGC.
         #
         @test (evaluate("<![CDATA[]] >")
-               == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                    E.DataContent("", L.Location("a buffer", -1)),
-                    E.MarkupError("ERROR: Expecting '>' to end a CDATA marked section.", [ ], L.Location("a buffer", -1)),
-                    E.CDATAMarkedSectionEnd(true, L.Location("a buffer", -1)),
-                    E.DataContent(" ", true, L.Location("a buffer", -1)),
-                    E.DataContent(">", false, L.Location("a buffer", -1)) ])
+               == [ CDataStart(L.Location("a buffer", -1)),
+                    DC("", L.Location("a buffer", -1)),
+                    ME("ERROR: Expecting '>' to end a CDATA marked section.", [ ], L.Location("a buffer", -1)),
+                    CDataEnd(true, L.Location("a buffer", -1)),
+                    DC(" ", true, L.Location("a buffer", -1)),
+                    DC(">", false, L.Location("a buffer", -1)) ])
+
         @test (evaluate("<![CDATA[Hello, World!]] >")
-               == [ E.CDATAMarkedSectionStart(L.Location("a buffer", -1)),
-                    E.DataContent("Hello, World!", L.Location("a buffer", -1)),
-                    E.MarkupError("ERROR: Expecting '>' to end a CDATA marked section.", [ ], L.Location("a buffer", -1)),
-                    E.CDATAMarkedSectionEnd(true, L.Location("a buffer", -1)),
-                    E.DataContent(" ", true, L.Location("a buffer", -1)),
-                    E.DataContent(">", false, L.Location("a buffer", -1)) ])
+               == [ CDataStart(L.Location("a buffer", -1)),
+                    DC("Hello, World!", L.Location("a buffer", -1)),
+                    ME("ERROR: Expecting '>' to end a CDATA marked section.", [ ], L.Location("a buffer", -1)),
+                    CDataEnd(true, L.Location("a buffer", -1)),
+                    DC(" ", true, L.Location("a buffer", -1)),
+                    DC(">", false, L.Location("a buffer", -1)) ])
     end
 end
