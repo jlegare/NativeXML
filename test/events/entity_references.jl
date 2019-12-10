9b3bd280493b26cb7e1a9eba239b1070411d3b69
Jacques Légaré <jacques.legare70@gmail.com>
2019-12-10 17:15:37 -0500

    Made these negative tests verify recovery.

--- test/events/entity_references.jl
+++ test/events/entity_references.jl
@@ -4,167 +4,180 @@
     E = Events
     L = Events.Lexical
 
-    @testset "Events/Entity References/Character References (Positive)" begin
-        @test evaluate("&#10;") == [ E.CharacterReference("10", L.Location("a buffer", -1)) ]
-        @test evaluate("&#xa;") == [ E.CharacterReference("xa", L.Location("a buffer", -1)) ]
+    CharRef = E.CharacterReference
+    GenRef  = E.EntityReferenceGeneral
+    ParRef  = E.EntityReferenceParameter
 
-        @test evaluate("&#10;&#xa;") == [ E.CharacterReference("10", L.Location("a buffer", -1)),
-                                          E.CharacterReference("xa", L.Location("a buffer", -1)) ]
-        @test evaluate("&#xa;&#10;") == [ E.CharacterReference("xa", L.Location("a buffer", -1)),
-                                          E.CharacterReference("10", L.Location("a buffer", -1)) ]
+    DC = E.DataContent
+    ME = E.MarkupError
+
+    @testset "Events/Entity References/Character References (Positive)" begin
+        @test evaluate("&#10;") == [ CharRef("10", L.Location("a buffer", -1)) ]
+        @test evaluate("&#xa;") == [ CharRef("xa", L.Location("a buffer", -1)) ]
+
+        @test evaluate("&#10;&#xa;") == [ CharRef("10", L.Location("a buffer", -1)),
+                                          CharRef("xa", L.Location("a buffer", -1)) ]
+        @test evaluate("&#xa;&#10;") == [ CharRef("xa", L.Location("a buffer", -1)),
+                                          CharRef("10", L.Location("a buffer", -1)) ]
 
         # This is a bogus character reference in terms of the XML specification, but legit at this layer of the parser.
         #
-        @test evaluate("&#xxx;") == [ E.CharacterReference("xxx", L.Location("a buffer", -1)) ]
+        @test evaluate("&#xxx;") == [ CharRef("xxx", L.Location("a buffer", -1)) ]
     end
 
     @testset "Events/Entity References/Character References (Negative ... no character specification)" begin
         # Check that a missing character specification is caught.
         #
-        @test (evaluate("&#;") == [ E.MarkupError("ERROR: Expecting a character value.",
-                                                  [ L.Token(L.cro, "&#", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
-                                    E.DataContent(";", false, L.Location("a buffer", -1)) ])
+        @test (evaluate("&#;") == [ ME("ERROR: Expecting a character value.",
+                                       [ L.Token(L.cro, "&#", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                                    DC(";", false, L.Location("a buffer", -1)) ])
 
         # Check that EOI is caught.
         #
-        @test (evaluate("&#") == [ E.MarkupError("ERROR: Expecting a character value.",
-                                                 [ L.Token(L.cro, "&#", L.Location("a buffer", -1)) ],
-                                                 L.Location("a buffer", -1)) ])
+        @test (evaluate("&#") == [ ME("ERROR: Expecting a character value.",
+                                      [ L.Token(L.cro, "&#", L.Location("a buffer", -1)) ],
+                                      L.Location("a buffer", -1)) ])
 
-        # Check that a random token is caught. But careful ... the parser keeps going, so we only check the FIRST
-        # event. (Otherwise we're testing the results of some other part of the parser.)
+        # Check that a random token is caught.
         #
         events = evaluate("&#<")
-        @test length(events) > 1
-        @test (events[1] == E.MarkupError("ERROR: Expecting a character value.",
-                                          [ L.Token(L.cro, "&#", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)))
+        @test length(events) >= 2
+        @test (events[1:2] == [ ME("ERROR: Expecting a character value.",
+                                   [ L.Token(L.cro, "&#", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                                ME("ERROR: Expecting an element name.",
+                                   [ L.Token(L.stago, "<", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/Entity References/Character References (Negative ... no terminator)" begin
         # Check that EOI is caught.
         #
         @test (evaluate("&#10")
-               == [ E.MarkupError("ERROR: Expecting ';' to end a character reference.",
-                                  [ L.Token(L.cro, "&#", L.Location("a buffer", -1)),
-                                    L.Token(L.text, "10", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
+               == [ ME("ERROR: Expecting ';' to end a character reference.",
+                       [ L.Token(L.cro, "&#", L.Location("a buffer", -1)),
+                         L.Token(L.text, "10", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
 
-        # Check that a random token is caught. But careful ... the parser keeps going, so we only check the FIRST
-        # event. (Otherwise we're testing the results of some other part of the parser.)
+        # Check that a random token is caught.
         #
         events = evaluate("&#10<")
-        @test length(events) > 1
-        @test (events[1] == E.MarkupError("ERROR: Expecting ';' to end a character reference.",
-                                          [ L.Token(L.cro, "&#", L.Location("a buffer", -1)),
-                                            L.Token(L.text, "10", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)))
+        @test length(events) >= 2
+        @test (events[1:2] == [ ME("ERROR: Expecting ';' to end a character reference.",
+                                   [ L.Token(L.cro, "&#", L.Location("a buffer", -1)),
+                                     L.Token(L.text, "10", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                                ME("ERROR: Expecting an element name.",
+                                   [ L.Token(L.stago, "<", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/Entity References/General Entity References (Positive)" begin
-        @test evaluate("&a;") == [ E.EntityReferenceGeneral("a", L.Location("a buffer", -1)) ]
-        @test evaluate("&é;") == [ E.EntityReferenceGeneral("é", L.Location("a buffer", -1)) ]
+        @test evaluate("&a;") == [ GenRef("a", L.Location("a buffer", -1)) ]
+        @test evaluate("&é;") == [ GenRef("é", L.Location("a buffer", -1)) ]
 
-        @test evaluate("&aé;") == [ E.EntityReferenceGeneral("aé", L.Location("a buffer", -1)) ]
-        @test evaluate("&éa;") == [ E.EntityReferenceGeneral("éa", L.Location("a buffer", -1)) ]
+        @test evaluate("&aé;") == [ GenRef("aé", L.Location("a buffer", -1)) ]
+        @test evaluate("&éa;") == [ GenRef("éa", L.Location("a buffer", -1)) ]
 
-        @test evaluate("&a;&é;") == [ E.EntityReferenceGeneral("a", L.Location("a buffer", -1)),
-                                      E.EntityReferenceGeneral("é", L.Location("a buffer", -1)) ]
+        @test evaluate("&a;&é;") == [ GenRef("a", L.Location("a buffer", -1)),
+                                      GenRef("é", L.Location("a buffer", -1)) ]
 
-        @test evaluate("&é;&a;") == [ E.EntityReferenceGeneral("é", L.Location("a buffer", -1)),
-                                      E.EntityReferenceGeneral("a", L.Location("a buffer", -1)) ]
+        @test evaluate("&é;&a;") == [ GenRef("é", L.Location("a buffer", -1)),
+                                      GenRef("a", L.Location("a buffer", -1)) ]
     end
 
     @testset "Events/Entity References/General Entity References (Negative ... no entity name)" begin
         # Check that a missing entity name is caught.
         #
         @test (evaluate("&;")
-               == [ E.MarkupError("ERROR: Expecting an entity name.",
-                                  [ L.Token(L.ero, "&", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
-                    E.DataContent(";", false, L.Location("a buffer", -1)) ])
+               == [ ME("ERROR: Expecting an entity name.",
+                       [ L.Token(L.ero, "&", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                    DC(";", false, L.Location("a buffer", -1)) ])
 
 
         # Check that EOI is caught.
         #
         @test (evaluate("&")
-               == [ E.MarkupError("ERROR: Expecting an entity name.",
+               == [ ME("ERROR: Expecting an entity name.",
                                   [ L.Token(L.ero, "&", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
 
-        # Check that a random token is caught. But careful ... the parser keeps going, so we only check the FIRST
-        # event. (Otherwise we're testing the results of some other part of the parser.)
+        # Check that a random token is caught.
         #
         events = evaluate("&<")
-        @test length(events) > 1
-        @test (events[1] == E.MarkupError("ERROR: Expecting an entity name.",
-                                          [ L.Token(L.ero, "&", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)))
+        @test length(events) >= 2
+        @test (events[1:2] == [ ME("ERROR: Expecting an entity name.",
+                                   [ L.Token(L.ero, "&", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                                ME("ERROR: Expecting an element name.",
+                                   [ L.Token(L.stago, "<", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/Entity References/General Entity References (Negative ... no terminator)" begin
         # Check that EOI is caught.
         #
         @test (evaluate("&a")
-               == [ E.MarkupError("ERROR: Expecting ';' to end an entity reference.",
-                                  [ L.Token(L.ero, "&", L.Location("a buffer", -1)),
-                                    L.Token(L.text, "a", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
+               == [ ME("ERROR: Expecting ';' to end an entity reference.",
+                       [ L.Token(L.ero, "&", L.Location("a buffer", -1)),
+                         L.Token(L.text, "a", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
 
-        # Check that a random token is caught. But careful ... the parser keeps going, so we only check the FIRST
-        # event. (Otherwise we're testing the results of some other part of the parser.)
+        # Check that a random token is caught.
         #
         events = evaluate("&a<")
-        @test length(events) > 1
-        @test (events[1] == E.MarkupError("ERROR: Expecting ';' to end an entity reference.",
-                                          [ L.Token(L.ero, "&", L.Location("a buffer", -1)),
-                                            L.Token(L.text, "a", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)))
+        @test length(events) >= 2
+        @test (events[1:2] == [ ME("ERROR: Expecting ';' to end an entity reference.",
+                                   [ L.Token(L.ero, "&", L.Location("a buffer", -1)),
+                                     L.Token(L.text, "a", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                                ME("ERROR: Expecting an element name.",
+                                   [ L.Token(L.stago, "<", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/Entity References/Parameter Entity References (Positive)" begin
-        @test evaluate("%a;") == [ E.EntityReferenceParameter("a", L.Location("a buffer", -1)) ]
-        @test evaluate("%é;") == [ E.EntityReferenceParameter("é", L.Location("a buffer", -1)) ]
+        @test evaluate("%a;") == [ ParRef("a", L.Location("a buffer", -1)) ]
+        @test evaluate("%é;") == [ ParRef("é", L.Location("a buffer", -1)) ]
 
-        @test evaluate("%aé;") == [ E.EntityReferenceParameter("aé", L.Location("a buffer", -1)) ]
-        @test evaluate("%éa;") == [ E.EntityReferenceParameter("éa", L.Location("a buffer", -1)) ]
+        @test evaluate("%aé;") == [ ParRef("aé", L.Location("a buffer", -1)) ]
+        @test evaluate("%éa;") == [ ParRef("éa", L.Location("a buffer", -1)) ]
 
-        @test evaluate("%a;%é;") == [ E.EntityReferenceParameter("a", L.Location("a buffer", -1)),
-                                                                  E.EntityReferenceParameter("é", L.Location("a buffer", -1)) ]
+        @test evaluate("%a;%é;") == [ ParRef("a", L.Location("a buffer", -1)),
+                                      ParRef("é", L.Location("a buffer", -1)) ]
 
-        @test evaluate("%é;%a;") == [ E.EntityReferenceParameter("é", L.Location("a buffer", -1)),
-                                                                  E.EntityReferenceParameter("a", L.Location("a buffer", -1)) ]
+        @test evaluate("%é;%a;") == [ ParRef("é", L.Location("a buffer", -1)),
+                                      ParRef("a", L.Location("a buffer", -1)) ]
     end
 
     @testset "Events/Entity References/Parameter Entity References (Negative ... no entity name)" begin
         # Check that a missing entity name is caught.
         #
-        @test (evaluate("%;") == [ E.MarkupError("ERROR: Expecting a parameter entity name.",
-                                                 [ L.Token(L.pero, "%", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
-                                   E.DataContent(";", false, L.Location("a buffer", -1)) ])
+        @test (evaluate("%;") == [ ME("ERROR: Expecting a parameter entity name.",
+                                      [ L.Token(L.pero, "%", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                                   DC(";", false, L.Location("a buffer", -1)) ])
 
         # Check that EOI is caught.
         #
-        @test (evaluate("%") == [ E.MarkupError("ERROR: Expecting a parameter entity name.",
-                                                [ L.Token(L.pero, "%", L.Location("a buffer", -1)) ],
-                                                L.Location("a buffer", -1)) ])
+        @test (evaluate("%") == [ ME("ERROR: Expecting a parameter entity name.",
+                                     [ L.Token(L.pero, "%", L.Location("a buffer", -1)) ],
+                                     L.Location("a buffer", -1)) ])
 
-        # Check that a random token is caught. But careful ... the parser keeps going, so we only check the FIRST
-        # event. (Otherwise we're testing the results of some other part of the parser.)
+        # Check that a random token is caught.
         #
         events = evaluate("%<")
-        @test length(events) > 1
-        @test (events[1] == E.MarkupError("ERROR: Expecting a parameter entity name.",
-                                          [ L.Token(L.pero, "%", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)))
+        @test length(events) >= 2
+        @test (events[1:2] == [ ME("ERROR: Expecting a parameter entity name.",
+                                   [ L.Token(L.pero, "%", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                                ME("ERROR: Expecting an element name.",
+                                   [ L.Token(L.stago, "<", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/Entity References/Parameter Entity References (Negative ... no terminator)" begin
         # Check that EOI is caught.
         #
-        @test (evaluate("%a") == [ E.MarkupError("ERROR: Expecting ';' to end a parameter entity reference.",
-                                                 [ L.Token(L.pero, "%", L.Location("a buffer", -1)),
-                                                   L.Token(L.text, "a", L.Location("a buffer", -1)) ],
-                                                 L.Location("a buffer", -1)) ])
+        @test (evaluate("%a") == [ ME("ERROR: Expecting ';' to end a parameter entity reference.",
+                                      [ L.Token(L.pero, "%", L.Location("a buffer", -1)),
+                                        L.Token(L.text, "a", L.Location("a buffer", -1)) ],
+                                      L.Location("a buffer", -1)) ])
 
-        # Check that a random token is caught. But careful ... the parser keeps going, so we only check the FIRST
-        # event. (Otherwise we're testing the results of some other part of the parser.)
+        # Check that a random token is caught.
         #
         events = evaluate("%a<")
-        @test length(events) > 1
-        @test (events[1] == E.MarkupError("ERROR: Expecting ';' to end a parameter entity reference.",
-                                          [ L.Token(L.pero, "%", L.Location("a buffer", -1)),
-                                            L.Token(L.text, "a", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)))
+        @test length(events) >= 2
+        @test (events[1:2] == [ ME("ERROR: Expecting ';' to end a parameter entity reference.",
+                                   [ L.Token(L.pero, "%", L.Location("a buffer", -1)),
+                                     L.Token(L.text, "a", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                                ME("ERROR: Expecting an element name.",
+                                   [ L.Token(L.stago, "<", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
     end
 end
