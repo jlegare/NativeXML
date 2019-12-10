6939dbda923f52d46352eaf2206d4c9f293308a1
Jacques Légaré <jacques.legare70@gmail.com>
2019-12-10 09:23:04 -0500

    These negative tests already verified recovery.

--- test/events/comments.jl
+++ test/events/comments.jl
@@ -4,101 +4,107 @@
     E = Events
     L = Events.Lexical
 
+    CommentStart = E.CommentStart
+    CommentEnd   = E.CommentEnd
+
+    DC = E.DataContent
+    ME = E.MarkupError
+
     @testset "Events/Comments (Positive)" begin
-        @test evaluate("<!---->") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                       E.DataContent("", L.Location("a buffer", -1)),
-                                       E.CommentEnd(L.Location("a buffer", -1)) ]
-        @test evaluate("<!--x-->") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                        E.DataContent("x", L.Location("a buffer", -1)),
-                                        E.CommentEnd(L.Location("a buffer", -1)) ]
-        @test evaluate("<!--xxx-->") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                          E.DataContent("xxx", L.Location("a buffer", -1)),
-                                          E.CommentEnd(L.Location("a buffer", -1)) ]
+        @test evaluate("<!---->") == [ CommentStart(L.Location("a buffer", -1)),
+                                       DC("", L.Location("a buffer", -1)),
+                                       CommentEnd(L.Location("a buffer", -1)) ]
+        @test evaluate("<!--x-->") == [ CommentStart(L.Location("a buffer", -1)),
+                                        DC("x", L.Location("a buffer", -1)),
+                                        CommentEnd(L.Location("a buffer", -1)) ]
+        @test evaluate("<!--xxx-->") == [ CommentStart(L.Location("a buffer", -1)),
+                                          DC("xxx", L.Location("a buffer", -1)),
+                                          CommentEnd(L.Location("a buffer", -1)) ]
 
-        @test (evaluate("<!--x--><!--y-->") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                                 E.DataContent("x", L.Location("a buffer", -1)),
-                                                 E.CommentEnd(L.Location("a buffer", -1)),
-                                                 E.CommentStart(L.Location("a buffer", -1)),
-                                                 E.DataContent("y", L.Location("a buffer", -1)),
-                                                 E.CommentEnd(L.Location("a buffer", -1)) ])
+        @test (evaluate("<!--x--><!--y-->") == [ CommentStart(L.Location("a buffer", -1)),
+                                                 DC("x", L.Location("a buffer", -1)),
+                                                 CommentEnd(L.Location("a buffer", -1)),
+                                                 CommentStart(L.Location("a buffer", -1)),
+                                                 DC("y", L.Location("a buffer", -1)),
+                                                 CommentEnd(L.Location("a buffer", -1)) ])
 
-        @test (evaluate("<!-- Hello, World! -->") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                                       E.DataContent(" Hello, World! ", L.Location("a buffer", -1)),
-                                                       E.CommentEnd(L.Location("a buffer", -1)) ])
+        @test (evaluate("<!-- Hello, World! -->") == [ CommentStart(L.Location("a buffer", -1)),
+                                                       DC(" Hello, World! ", L.Location("a buffer", -1)),
+                                                       CommentEnd(L.Location("a buffer", -1)) ])
 
-        @test (evaluate("<!--é-->") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                         E.DataContent("é", L.Location("a buffer", -1)),
-                                         E.CommentEnd(L.Location("a buffer", -1)) ])
+        @test (evaluate("<!--é-->") == [ CommentStart(L.Location("a buffer", -1)),
+                                         DC("é", L.Location("a buffer", -1)),
+                                         CommentEnd(L.Location("a buffer", -1)) ])
 
         # This is from the XML specification, § 2.5.
         #
         @test (evaluate("<!-- declarations for <head> & <body> -->")
-               == [ E.CommentStart(L.Location("a buffer", -1)),
-                    E.DataContent(" declarations for <head> & <body> ", L.Location("a buffer", -1)),
-                    E.CommentEnd(L.Location("a buffer", -1)) ])
+               == [ CommentStart(L.Location("a buffer", -1)),
+                    DC(" declarations for <head> & <body> ", L.Location("a buffer", -1)),
+                    CommentEnd(L.Location("a buffer", -1)) ])
 
-        @test (evaluate("<!-- &e; -->") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                             E.DataContent(" &e; ", L.Location("a buffer", -1)),
-                                             E.CommentEnd(L.Location("a buffer", -1)) ])
-        @test (evaluate("<!-- <?target value?> -->") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                                          E.DataContent(" <?target value?> ", L.Location("a buffer", -1)),
-                                                          E.CommentEnd(L.Location("a buffer", -1)) ])
+        @test (evaluate("<!-- &e; -->") == [ CommentStart(L.Location("a buffer", -1)),
+                                             DC(" &e; ", L.Location("a buffer", -1)),
+                                             CommentEnd(L.Location("a buffer", -1)) ])
+        @test (evaluate("<!-- <?target value?> -->") == [ CommentStart(L.Location("a buffer", -1)),
+                                                          DC(" <?target value?> ", L.Location("a buffer", -1)),
+                                                          CommentEnd(L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/Comments (Negative ... no terminator)" begin
-        @test (evaluate("<!--") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                     E.DataContent("", L.Location("a buffer", -1)),
-                                     E.MarkupError("ERROR: Expecting '-->' to end a comment.", [ ], L.Location("a buffer", -1)),
-                                     E.CommentEnd(true, L.Location("a buffer", -1)) ])
-        @test (evaluate("<!--x") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                      E.DataContent("x", L.Location("a buffer", -1)),
-                                      E.MarkupError("ERROR: Expecting '-->' to end a comment.", [ ], L.Location("a buffer", -1)),
-                                      E.CommentEnd(true, L.Location("a buffer", -1)) ])
-        @test (evaluate("<!--é") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                      E.DataContent("é", L.Location("a buffer", -1)),
-                                      E.MarkupError("ERROR: Expecting '-->' to end a comment.", [ ], L.Location("a buffer", -1)),
-                                      E.CommentEnd(true, L.Location("a buffer", -1)) ])
+        @test (evaluate("<!--") == [ CommentStart(L.Location("a buffer", -1)),
+                                     DC("", L.Location("a buffer", -1)),
+                                     ME("ERROR: Expecting '-->' to end a comment.", [ ], L.Location("a buffer", -1)),
+                                     CommentEnd(true, L.Location("a buffer", -1)) ])
+        @test (evaluate("<!--x") == [ CommentStart(L.Location("a buffer", -1)),
+                                      DC("x", L.Location("a buffer", -1)),
+                                      ME("ERROR: Expecting '-->' to end a comment.", [ ], L.Location("a buffer", -1)),
+                                      CommentEnd(true, L.Location("a buffer", -1)) ])
+        @test (evaluate("<!--é") == [ CommentStart(L.Location("a buffer", -1)),
+                                      DC("é", L.Location("a buffer", -1)),
+                                      ME("ERROR: Expecting '-->' to end a comment.", [ ], L.Location("a buffer", -1)),
+                                      CommentEnd(true, L.Location("a buffer", -1)) ])
         @test (evaluate("<!-- declarations for <head> & <body> ")
-               == [ E.CommentStart(L.Location("a buffer", -1)),
-                    E.DataContent(" declarations for <head> & <body> ", L.Location("a buffer", -1)),
-                    E.MarkupError("ERROR: Expecting '-->' to end a comment.", [ ], L.Location("a buffer", -1)),
-                    E.CommentEnd(true, L.Location("a buffer", -1)) ])
+               == [ CommentStart(L.Location("a buffer", -1)),
+                    DC(" declarations for <head> & <body> ", L.Location("a buffer", -1)),
+                    ME("ERROR: Expecting '-->' to end a comment.", [ ], L.Location("a buffer", -1)),
+                    CommentEnd(true, L.Location("a buffer", -1)) ])
 
-        @test (evaluate("<!--x-") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                       E.DataContent("x-", L.Location("a buffer", -1)),
-                                       E.MarkupError("ERROR: Expecting '-->' to end a comment.", [ ], L.Location("a buffer", -1)),
-                                       E.CommentEnd(true, L.Location("a buffer", -1)) ])
-        @test (evaluate("<!--x--") == [ E.CommentStart(L.Location("a buffer", -1)),
-                                        E.DataContent("x", L.Location("a buffer", -1)),
-                                        E.MarkupError("ERROR: Expecting '-->' to end a comment.", [ ], L.Location("a buffer", -1)),
-                                        E.CommentEnd(true, L.Location("a buffer", -1)) ])
+        @test (evaluate("<!--x-") == [ CommentStart(L.Location("a buffer", -1)),
+                                       DC("x-", L.Location("a buffer", -1)),
+                                       ME("ERROR: Expecting '-->' to end a comment.", [ ], L.Location("a buffer", -1)),
+                                       CommentEnd(true, L.Location("a buffer", -1)) ])
+        @test (evaluate("<!--x--") == [ CommentStart(L.Location("a buffer", -1)),
+                                        DC("x", L.Location("a buffer", -1)),
+                                        ME("ERROR: Expecting '-->' to end a comment.", [ ], L.Location("a buffer", -1)),
+                                        CommentEnd(true, L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/Comments (Negative ... nested --)" begin
         @test (evaluate("<!-- -- -->")
-               == [ E.CommentStart(L.Location("a buffer", -1)),
-                    E.DataContent(" ", L.Location("a buffer", -1)),
-                    E.MarkupError("ERROR: '--' is not allowed inside a comment.", [ ], L.Location("a buffer", -1)),
-                    E.CommentEnd(true, L.Location("a buffer", -1)),
-                    E.DataContent(" ", true, L.Location("a buffer", -1)),
-                    E.DataContent("--", false, L.Location("a buffer", -1)),
-                    E.DataContent(">", false, L.Location("a buffer", -1)) ])
+               == [ CommentStart(L.Location("a buffer", -1)),
+                    DC(" ", L.Location("a buffer", -1)),
+                    ME("ERROR: '--' is not allowed inside a comment.", [ ], L.Location("a buffer", -1)),
+                    CommentEnd(true, L.Location("a buffer", -1)),
+                    DC(" ", true, L.Location("a buffer", -1)),
+                    DC("--", false, L.Location("a buffer", -1)),
+                    DC(">", false, L.Location("a buffer", -1)) ])
         @test (evaluate("<!------>")
-               == [ E.CommentStart(L.Location("a buffer", -1)),
-                    E.DataContent("", L.Location("a buffer", -1)),
-                    E.MarkupError("ERROR: '--' is not allowed inside a comment.", [ ], L.Location("a buffer", -1)),
-                    E.CommentEnd(true, L.Location("a buffer", -1)),
-                    E.DataContent("--", false, L.Location("a buffer", -1)),
-                    E.DataContent(">", false, L.Location("a buffer", -1)) ])
+               == [ CommentStart(L.Location("a buffer", -1)),
+                    DC("", L.Location("a buffer", -1)),
+                    ME("ERROR: '--' is not allowed inside a comment.", [ ], L.Location("a buffer", -1)),
+                    CommentEnd(true, L.Location("a buffer", -1)),
+                    DC("--", false, L.Location("a buffer", -1)),
+                    DC(">", false, L.Location("a buffer", -1)) ])
 
         # This is from the XML specification, § 2.5.
         #
         @test (evaluate("<!-- B+, B, or B--->")
-               == [ E.CommentStart(L.Location("a buffer", -1)),
-                    E.DataContent(" B+, B, or B", L.Location("a buffer", -1)),
-                    E.MarkupError("ERROR: '--' is not allowed inside a comment.", [ ], L.Location("a buffer", -1)),
-                    E.CommentEnd(true, L.Location("a buffer", -1)),
-                    E.DataContent("-", false, L.Location("a buffer", -1)),
-                    E.DataContent(">", false, L.Location("a buffer", -1)) ])
+               == [ CommentStart(L.Location("a buffer", -1)),
+                    DC(" B+, B, or B", L.Location("a buffer", -1)),
+                    ME("ERROR: '--' is not allowed inside a comment.", [ ], L.Location("a buffer", -1)),
+                    CommentEnd(true, L.Location("a buffer", -1)),
+                    DC("-", false, L.Location("a buffer", -1)),
+                    DC(">", false, L.Location("a buffer", -1)) ])
     end
 end
