4bb59daea1ff830c18dcfd72d4fd9bce90d5c7dc
Jacques Légaré <jacques.legare70@gmail.com>
2019-12-10 15:12:57 -0500

    Made these negative tests verify recovery.

--- test/events/element_end.jl
+++ test/events/element_end.jl
@@ -4,64 +4,72 @@
     E = Events
     L = Events.Lexical
 
+    ElementEnd   = E.ElementEnd
+    ElementStart = E.ElementStart
+
+    DC = E.DataContent
+    ME = E.MarkupError
+
     @testset "Events/Element Ends (Positive)" begin
-        @test evaluate("</a>")   == [ E.ElementEnd("a", L.Location("a buffer", -1)) ]
-        @test evaluate("</abc>") == [ E.ElementEnd("abc", L.Location("a buffer", -1)) ]
-        @test evaluate("</é>")   == [ E.ElementEnd("é", L.Location("a buffer", -1)) ]
-        @test evaluate("</aé>")  == [ E.ElementEnd("aé", L.Location("a buffer", -1)) ]
-        @test evaluate("</éa>")  == [ E.ElementEnd("éa", L.Location("a buffer", -1)) ]
+        @test evaluate("</a>")   == [ ElementEnd("a", L.Location("a buffer", -1)) ]
+        @test evaluate("</abc>") == [ ElementEnd("abc", L.Location("a buffer", -1)) ]
+        @test evaluate("</é>")   == [ ElementEnd("é", L.Location("a buffer", -1)) ]
+        @test evaluate("</aé>")  == [ ElementEnd("aé", L.Location("a buffer", -1)) ]
+        @test evaluate("</éa>")  == [ ElementEnd("éa", L.Location("a buffer", -1)) ]
 
         # White space is allowed after the element name. Just try various combinations, and check that it is discarded.
         #
-        @test evaluate("</a >")       == [ E.ElementEnd("a", L.Location("a buffer", -1)) ]
-        @test evaluate("</a\u09>")    == [ E.ElementEnd("a", L.Location("a buffer", -1)) ]
-        @test evaluate("</a	\n>") == [ E.ElementEnd("a", L.Location("a buffer", -1)) ]
+        @test evaluate("</a >")       == [ ElementEnd("a", L.Location("a buffer", -1)) ]
+        @test evaluate("</a\u09>")    == [ ElementEnd("a", L.Location("a buffer", -1)) ]
+        @test evaluate("</a	\n>") == [ ElementEnd("a", L.Location("a buffer", -1)) ]
 
-        @test evaluate("</a></b>") == [ E.ElementEnd("a", L.Location("a buffer", -1)),
-                                        E.ElementEnd("b", L.Location("a buffer", -1)) ]
+        @test evaluate("</a></b>") == [ ElementEnd("a", L.Location("a buffer", -1)),
+                                        ElementEnd("b", L.Location("a buffer", -1)) ]
     end
 
     @testset "Events/Element Ends (Negative ... no terminator)" begin
         @test (evaluate("</a")
-               == [ E.MarkupError("ERROR: Expecting '>' to end an element close tag.", [ ], L.Location("a buffer", -1)),
-                    E.ElementEnd(true, "a", L.Location("a buffer", -1)) ])
+               == [ ME("ERROR: Expecting '>' to end an element close tag.", [ ], L.Location("a buffer", -1)),
+                    ElementEnd(true, "a", L.Location("a buffer", -1)) ])
 
         @test (evaluate("</abc")
-               == [ E.MarkupError("ERROR: Expecting '>' to end an element close tag.", [ ], L.Location("a buffer", -1)),
-                    E.ElementEnd(true, "abc", L.Location("a buffer", -1)) ])
+               == [ ME("ERROR: Expecting '>' to end an element close tag.", [ ], L.Location("a buffer", -1)),
+                    ElementEnd(true, "abc", L.Location("a buffer", -1)) ])
 
         @test (evaluate("</a ")
-               == [ E.MarkupError("ERROR: Expecting '>' to end an element close tag.", [ ], L.Location("a buffer", -1)),
-                    E.ElementEnd(true, "a", L.Location("a buffer", -1)) ])
+               == [ ME("ERROR: Expecting '>' to end an element close tag.", [ ], L.Location("a buffer", -1)),
+                    ElementEnd(true, "a", L.Location("a buffer", -1)) ])
 
-        # Check that a random token is caught. But careful ... the parser keeps going, so we only check the FIRST
-        # event. (Otherwise we're testing the results of some other part of the parser.)
-        #
         events = evaluate("</a<?")
-        @test length(events) > 1
-        @test (events[1] == E.MarkupError("ERROR: Expecting '>' to end an element close tag.", [ ], L.Location("a buffer", -1)))
+        @test length(events) >= 3
+        @test (events[1:3] == [ ME("ERROR: Expecting '>' to end an element close tag.", [ ], L.Location("a buffer", -1)),
+                                ElementEnd(true, "a", L.Location("a buffer", -1)),
+                                ME("ERROR: Expecting a PI target.", [ L.Token(L.pio, "<?", L.Location("a buffer", -1)) ],
+                                   L.Location("a buffer", -1)) ])
 
         events = evaluate("</a <?")
-        @test length(events) > 1
-        @test (events[1] == E.MarkupError("ERROR: Expecting '>' to end an element close tag.", [ ], L.Location("a buffer", -1)))
+        @test length(events) >= 3
+        @test (events[1:3] == [ ME("ERROR: Expecting '>' to end an element close tag.", [ ], L.Location("a buffer", -1)),
+                                ElementEnd(true, "a", L.Location("a buffer", -1)),
+                                ME("ERROR: Expecting a PI target.", [ L.Token(L.pio, "<?", L.Location("a buffer", -1)) ],
+                                   L.Location("a buffer", -1)) ])
     end
 
     @testset "Events/Element Ends (Negative ... no element name)" begin
-        @test (evaluate("</")
-               == [ E.MarkupError("ERROR: Expecting an element name.",
-                                  [ L.Token(L.etago, "</", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
+        @test (evaluate("</") == [ ME("ERROR: Expecting an element name.",
+                                      [ L.Token(L.etago, "</", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)) ])
 
-        # Check that a random token is caught. But careful ... the parser keeps going, so we only check the FIRST
-        # event. (Otherwise we're testing the results of some other part of the parser.)
-        #
         events = evaluate("</<?")
-        @test length(events) > 1
-        @test (events[1] == E.MarkupError("ERROR: Expecting an element name.",
-                                          [ L.Token(L.etago, "</", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)))
+        @test length(events) >= 2
+        @test (events[1:2] == [ ME("ERROR: Expecting an element name.",
+                                   [ L.Token(L.etago, "</", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                                ME("ERROR: Expecting a PI target.", [ L.Token(L.pio, "<?", L.Location("a buffer", -1)) ],
+                                   L.Location("a buffer", -1)) ])
 
         events = evaluate("</ ")
-        @test length(events) > 1
-        @test (events[1] == E.MarkupError("ERROR: Expecting an element name.",
-                                          [ L.Token(L.etago, "</", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)))
+        @test length(events) >= 2
+        @test (events[1:2] == [ ME("ERROR: Expecting an element name.",
+                                   [ L.Token(L.etago, "</", L.Location("a buffer", -1)) ], L.Location("a buffer", -1)),
+                                DC(" ", true, L.Location("a buffer", -1)) ])
     end
 end
